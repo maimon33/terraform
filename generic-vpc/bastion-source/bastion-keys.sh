83c923e37c925780a313d602e32c2caeec4ab734
Assi Maimon <maimon33@gmail.com>
2020-07-02 11:26:27 +0300

    wip

--- generic-vpc/bastion-source/bastion-keys.sh
+++ generic-vpc/bastion-source/bastion-keys.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Revert to orig authorized_keys files and create if for the fisrt time
-cat /home/ubuntu/.ssh/authorized_keys.orig > /home/ubuntu/.ssh/authorized_keys 2>/dev/null
+if [ -f "/home/ubuntu/.ssh/authorized_keys.orig" ]; then
+    cat /home/ubuntu/.ssh/authorized_keys.orig > /home/ubuntu/.ssh/authorized_keys
+fi
 cp /home/ubuntu/.ssh/authorized_keys /home/ubuntu/.ssh/authorized_keys.orig
 
 export BACKEND_BUCKET=SED_BUCKET
 mkdir -p /home/ubuntu/.ssh/keys && rm -rf /home/ubuntu/.ssh/keys/*
 aws s3 cp s3://$BACKEND_BUCKET/keys/ /home/ubuntu/.ssh/keys/. --recursive --exclude id_rsa
 
-export PUB_KEYS=/home/ubuntu/.ssh/keys/
+export PUB_KEYS=/home/ubuntu/.ssh/keys/*
 for f in $PUB_KEYS
 do
   echo "Processing $f file..."
